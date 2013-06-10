574781f19e1a433331695a5e123c50f3d147da37
julio <julio@10.230.168.102>
2013-06-11 00:11:54 +0100

    Prepared for Exam

--- src/examen2/Contact.java
+++ src/examen2/Contact.java
@@ -1,20 +1,5 @@
 package examen2;
 
-public class Contact extends Entity{
-    private String phoneNumber;
-
-    public Contact(String name, String phoneNumber) {
-        super(name);
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    
-    @Override
-    public void receiveMessage(String message) {
-        MessengerTools.sendMessage(message, phoneNumber);
-    }
+public class Contact {
     
 }
--- src/examen2/Entity.java
+++ src/examen2/Entity.java
@@ -1,22 +1,5 @@
 package examen2;
 
-public abstract class Entity{
-    private String name;
-    private int id;
-
-    public Entity(String name) {
-        this.name = name;
-        this.id= MessengerTools.getUniqueId();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getId() {
-        return id;
-    }
-    
-    public abstract void receiveMessage(String message);
-        
+public class Entity{
+         
 }
